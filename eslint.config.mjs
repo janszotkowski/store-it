597a6668134e0dabffa9a93aeae905f32d3e2fff
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-02-16 19:29:29 +0100

    set better eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,95 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import nextPlugin from '@next/eslint-plugin-next';
+import stylisticPlugin from '@stylistic/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import interfaceToTypePlugin from 'eslint-plugin-interface-to-type';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/**
+ * @type {import('eslint').Linter.FlatConfig[]}
+ */
+export default [
+    {
+        ignores: ['node_modules', 'dist', '**/.next/**'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                exports: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+                process: 'readonly',
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'jsx-quotes': ['error', 'prefer-single'],
-      'react/jsx-curly-brace-presence': 'off',
+                window: 'readonly',
+                document: 'readonly',
+                console: 'readonly',
+            },
+        },
     },
-  },
-];
 
-export default eslintConfig;
+    js.configs.recommended,
+
+    {
+        plugins: {
+            '@next/next': nextPlugin
+        }
+    },
+
+    {
+        plugins: {
+            react: reactPlugin,
+            'react-hooks': reactHooksPlugin,
+        },
+        rules: {
+            ...reactPlugin.configs.recommended.rules,
+            ...reactHooksPlugin.configs.recommended.rules,
+        },
+    },
+
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: './tsconfig.json',
+                tsconfigRootDir: process.cwd(),
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+        },
+    },
+
+    {
+        ignores: ['.next'],
+        plugins: {
+            stylistic: stylisticPlugin,
+        },
+        rules: {
+            ...stylisticPlugin.configs.recommended,
+        },
+    },
+
+    {
+        ignores: ['.next'],
+        rules: {
+            'stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+        },
+    },
+
+    {
+        plugins: {
+            'interface-to-type': interfaceToTypePlugin,
+        },
+        rules: {
+            'interface-to-type/prefer-type-over-interface': 'error',
+        },
+    },
+];
