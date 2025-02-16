b8129ddc76a3aede82e65ffa6ba2882d8a3b6319
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-02-16 20:59:31 +0100

    create auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,8 +6,13 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = (props: LayoutProps): React.ReactElement => {
     return (
-        <div>
-            {props.children}
+        <div className={'flex min-h-screen bg-blue-100 p-2'}>
+            <section className={'flex-1 p-10 text-white'}>
+
+            </section>
+            <section className={'rounded-2xl bg-white p-10'}>
+                {props.children}
+            </section>
         </div>
     );
 }
