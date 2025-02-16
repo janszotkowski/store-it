45b7a32a317a039aaac1cb604f3eeaf4714c9168
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-02-16 22:23:37 +0100

    basic auth style

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,9 +6,12 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = (props: LayoutProps): React.ReactElement => {
     return (
-        <div className={'flex min-h-screen bg-blue-100 p-2'}>
+        <div className={'flex min-h-screen bg-blue-100 p-3'}>
             <section className={'flex-1 p-10 text-white'}>
-
+                Welcome to StoreIt!
+                Store your files securely and access them from anywhere.
+                Sign in to continue or create a new account in just a few steps.
+                <img src={'/illustration.svg'} alt={'StoreIt Logo'} className={'size-96'}/>
             </section>
             <section className={'rounded-2xl bg-white p-10'}>
                 {props.children}
