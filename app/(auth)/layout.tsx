d895647561d1cb2ae525c13c2554a5dc441a326d
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-02-17 12:03:41 +0100

    auth layout wip

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,12 +8,15 @@ const Layout: React.FC<LayoutProps> = (props: LayoutProps): React.ReactElement =
     return (
         <div className={'flex min-h-screen bg-blue-100 p-3'}>
             <section className={'flex-1 p-10 text-white'}>
-                Welcome to StoreIt!
-                Store your files securely and access them from anywhere.
-                Sign in to continue or create a new account in just a few steps.
-                <img src={'/illustration.svg'} alt={'StoreIt Logo'} className={'size-96'}/>
+                <div className={'max-w-xl'}>
+                    <h1>Welcome to StoreIt!</h1>
+
+                    Store your files securely and access them from anywhere.
+                    Sign in to continue or create a new account in just a few steps.
+                    <img src={'/illustration.svg'} alt={'StoreIt Logo'} className={'size-96'}/>
+                </div>
             </section>
-            <section className={'rounded-2xl bg-white p-10'}>
+            <section className={'w-[600px] rounded-2xl bg-white p-10'}>
                 {props.children}
             </section>
         </div>
