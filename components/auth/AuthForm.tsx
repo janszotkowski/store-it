da9053feee76a92fda4ec9cc37bddcd939699223
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-03-06 22:08:43 +0100

    better form zod schema

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -7,14 +7,19 @@ import { z } from 'zod';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-    email: z.string().email()
-});
+type FormType = 'sign-in' | 'sign-up';
 
 type AuthFormProps = {
-    type: 'sign-in' | 'sign-up';
+    type: FormType;
+};
+
+const authFormSchema = (formType: FormType) => {
+    return z.object({
+        email: z.string().email(),
+        userName: formType === 'sign-up' ? z.string().min(2).max(50) : z.string().optional()
+    });
 };
 
 export const AuthForm: React.FC<AuthFormProps> = (props: AuthFormProps): React.ReactElement => {
@@ -25,10 +30,11 @@ export const AuthForm: React.FC<AuthFormProps> = (props: AuthFormProps): React.R
         setErrorMessage(undefined);
     }, []);
 
+    const formSchema = authFormSchema(props.type);
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: '',
+            userName: '',
             email: ''
         }
     });
@@ -43,14 +49,14 @@ export const AuthForm: React.FC<AuthFormProps> = (props: AuthFormProps): React.R
     return (
         <Form {...form}>
             <h1 className={'h1 mb-12 mx-auto w-max'}>
-                StoreIt
+                {props.type === 'sign-in' ? 'Sign in' : 'Sign up'}
             </h1>
             <form onSubmit={form.handleSubmit(onSubmit)} className={'space-y-8'}>
                 {
                     props.type == 'sign-up' &&
                     <FormField
                         control={form.control}
-                        name={'username'}
+                        name={'userName'}
                         render={({field}) => (
                             <FormItem>
                                 <div className={'!shad-form-item'}>
@@ -101,6 +107,18 @@ export const AuthForm: React.FC<AuthFormProps> = (props: AuthFormProps): React.R
                     errorMessage &&
                     <p className={'error-message'}>* {errorMessage}</p>
                 }
+
+                <div className={'flex justify-center'}>
+                    <p>
+                        {props.type === 'sign-in' ? 'Don\'t have an account ?' : 'Already have an account ?'}
+                    </p>
+                    <Link
+                        className={'ml-1 font-medium text-blue-60 hover:text-blue-80'}
+                        href={props.type === 'sign-in' ? '/sign-up' : '/sign-in'}
+                    >
+                        {props.type === 'sign-in' ? 'Sign up' : 'Sign in'}
+                    </Link>
+                </div>
             </form>
         </Form>
     );
